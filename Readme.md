d07065f4c964fdb6e73639dd7ca685c97128ffa6
uramer <antonuramer@gmail.com>
2019-04-02 21:58:44 +0200

    init

---
+++ Readme.md
@@ -0,0 +1,18 @@
+A collection of popular improvements to vanilla leveling mechanics. Fixed attribute modifiers, level cap, rescaled base health, "natural" progression - gain attributes by increasing skills.
+
+Requires [DataManager](https://github.com/tes3mp-scripts/DataManager)!
+
+You can find the configuration file in `server/data/custom/__config_BetterVanillaLeveling.json`.
+* `attributeModifier` modifier players have on level up for all attributes but luck. Should be from `1` to `5`.
+* `luckModifier` modifier players have on level up for luck. Should be from `1` to `5`.
+* `levelCap` limits the maximal allowed level. `100` is the default.
+* `healthScale` sets health to `level * levelModifier + endurance * enduranceModifier + strength * strengthModifier`
+  * `enabled` whether health scaling should change.
+  * `enduranceModifier` endurance coefficient in the health scaling formula.
+  * `strengthModifier` strength coefficient in the health scaling formula.
+  * `levelModifier` level coefficient in the health scaling formula.
+* `progression`
+  * `enabled` whether "natural" progression should be enabled.
+  * `threshold` how many skill increases are required for an attribute increase.
+  * `attributePoints` by how many points does the attribute incraese after `threshold` is passed.
+  * `message` message displayed when an attribute is increased this way.
---
+++ main.lua
@@ -0,0 +1,160 @@
+local Leveling = {}
+
+Leveling.scriptName = "BetterVanillaLeveling"
+
+Leveling.defaultConfig = {
+    attributeModifier = 2,
+    luckModifier = 1,
+    levelCap = 100,
+    healthScale = {
+        enabled = true,
+        enduranceModifier = 1.5,
+        strengthModifier = 0.2,
+        levelModifier = 1.5
+    },
+    progression = {
+        enabled = true,
+        threshold = 10,
+        attributePoints = 1,
+        message = "#FFD700Rigorous training has improved your %s by %d!\n"
+    }
+}
+
+Leveling.config = DataManager.loadConfiguration(Leveling.scriptName, Leveling.defaultConfig)
+
+Leveling.ENDURANCE = tes3mp.GetAttributeId("Endurance")
+Leveling.STRENGTH = tes3mp.GetAttributeId("Strength")
+
+Leveling.modifierToSkillIncrease = {0, 1, 5, 8, 10}
+
+function Leveling.updateSkillIncrease()
+    Leveling.skillIncrease = {}
+    for id = 0, 6 do
+        Leveling.skillIncrease[id] = 
+            Leveling.modifierToSkillIncrease[Leveling.config.attributeModifier]
+    end
+
+    Leveling.skillIncrease[7] = 
+            Leveling.modifierToSkillIncrease[Leveling.config.luckModifier]
+end
+
+Leveling.updateSkillIncrease()
+
+
+function Leveling.increaseAttribute(pid, id, value)
+    local name = tes3mp.GetAttributeName(id)
+    Players[pid].data.attributes[name] = Players[pid].data.attributes[name] + value
+    Players[pid]:LoadAttributes()
+end
+
+function Leveling.progression(pid)
+    local player = Players[pid]
+    local progress = player.data.customVariables.levelingProgression
+
+    if progress == nil then
+        progress = {}
+        for id = 0, 7 do
+            progress[id] = 0
+        end
+
+        player.data.customVariables.levelingProgression = progress
+    end
+
+    local attributesChanged = false
+
+    for id = 0, 7 do
+        progress[id] = progress[id] + math.max(tes3mp.GetSkillIncrease(pid, id) - Leveling.skillIncrease[id], 0)
+
+        if progress[id] >= Leveling.config.progression.threshold then
+            progress[id] = progress[id] - Leveling.config.progression.threshold
+
+            tes3mp.SendMessage(
+                pid,
+                string.format(
+                    Leveling.config.progression.message,
+                    tes3mp.GetAttributeName(id),
+                    Leveling.config.progression.attributePoints
+                )
+            )
+
+            if id == Leveling.STRENGTH or id == Leveling.ENDURANCE then
+                attributesChanged = true
+            end
+            Leveling.increaseAttribute(pid, id, Leveling.config.progression.attributePoints)
+        end
+    end
+
+    if attributesChanged then
+        Leveling.scaleBaseHealth(pid)
+    end
+end
+
+function Leveling.resetSkillIncreases(pid)
+    local name = nil
+    for id = 0, 7 do
+        name = tes3mp.GetAttributeName(id)
+        Players[pid].data.attributeSkillIncreases[name] = Leveling.skillIncrease[id]
+        tes3mp.SetSkillIncrease(pid, id, Leveling.skillIncrease[id])
+        tes3mp.SendAttributes(pid)
+    end
+end
+
+function Leveling.applyLevelCap(pid)
+    local level = tes3mp.GetLevel(pid)
+    if level >= Leveling.config.levelCap then
+        tes3mp.SetLevelProgress(pid, 0)
+        tes3mp.SendLevel(pid)
+        Players[pid].data.stats.levelProgress = 0
+    end
+end
+
+function Leveling.scaleBaseHealth(pid)
+    if not Leveling.config.healthScale.enabled then
+        return
+    end
+    local endurance = tes3mp.GetAttributeBase(pid, Leveling.ENDURANCE)
+    local strength = tes3mp.GetAttributeBase(pid, Leveling.STRENGTH)
+    local level = tes3mp.GetLevel(pid)
+    local health = level * Leveling.config.healthScale.levelModifier +
+        endurance * Leveling.config.healthScale.enduranceModifier +
+        strength * Leveling.config.healthScale.strengthModifier
+
+    local relativeHealth = tes3mp.GetHealthCurrent(pid) / tes3mp.GetHealthBase(pid)
+    if relativeHealth <= 1 then
+        tes3mp.SetHealthCurrent(pid, relativeHealth * health)
+        tes3mp.SendStatsDynamic(pid)
+    end
+    
+    Players[pid]:SetHealthBase(health)
+end
+
+
+function Leveling.OnPlayerSkill(eventStatus, pid)
+    if eventStatus.validCustomHandlers then
+        --Leveling.progression(pid)
+        --[[Leveling.resetSkillIncreases(pid)
+        if Leveling.config.levelCap < 100 then
+            Leveling.applyLevelCap(pid)
+        end]]
+    end
+end
+
+function Leveling.OnPlayerLevel(eventStatus, pid)
+    if eventStatus.validCustomHandlers then
+        Leveling.progression(pid)
+        Leveling.resetSkillIncreases(pid)
+        Leveling.scaleBaseHealth(pid)
+    end
+end
+
+function Leveling.OnPlayerAuthentified(eventStatus, pid)
+    if eventStatus.validCustomHandlers then
+        Leveling.scaleBaseHealth(pid)
+        Leveling.resetSkillIncreases(pid)
+    end
+end
+
+
+customEventHooks.registerHandler("OnPlayerSkill", Leveling.OnPlayerSkill)
+customEventHooks.registerHandler("OnPlayerLevel", Leveling.OnPlayerLevel)
+customEventHooks.registerHandler("OnPlayerAuthentified", Leveling.OnPlayerAuthentified)
